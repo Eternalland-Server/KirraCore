8214baaa791b0253c8411aa7a63e8b10e4854adb
kirraObj <c@skymc.me>
2022-07-19 16:04:58 +0800

    update.

--- platform/platform-bukkit/src/main/java/net/sakuragame/eternal/kirracore/bukkit/command/management/CommandTeleportServer.java
+++ platform/platform-bukkit/src/main/java/net/sakuragame/eternal/kirracore/bukkit/command/management/CommandTeleportServer.java
@@ -1,13 +1,32 @@
 package net.sakuragame.eternal.kirracore.bukkit.command.management;
 
 import com.qrakn.honcho.command.CommandMeta;
+import lombok.val;
 import net.sakuragame.eternal.kirracore.bukkit.KirraCoreBukkitAPI;
 import net.sakuragame.eternal.kirracore.common.packet.impl.sub.AssignType;
+import org.bukkit.Bukkit;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 @SuppressWarnings("SpellCheckingInspection")
 @CommandMeta(label = {"teleportServer", "tpServer"}, permission = "admin", async = true)
 public class CommandTeleportServer {
+
+    public void execute(CommandSender sender, String playerID, String serverIDOrPrefix) {
+        val player = Bukkit.getPlayerExact(playerID);
+        if (player == null) {
+            return;
+        }
+        execute(player, serverIDOrPrefix);
+    }
+
+    public void execute(CommandSender sender, String playerID, String serverIDOrPrefix, String assignType, String assignValue) {
+        val player = Bukkit.getPlayerExact(playerID);
+        if (player == null) {
+            return;
+        }
+        execute(player, serverIDOrPrefix, assignType, assignValue);
+    }
 
     public void execute(Player player, String serverIDOrPrefix) {
         KirraCoreBukkitAPI.teleportPlayerToServerByBalancing(serverIDOrPrefix, player.getUniqueId());
