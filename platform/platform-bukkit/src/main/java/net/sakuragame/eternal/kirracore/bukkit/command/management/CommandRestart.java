4a74a3d423b904cb7261db8ae9ae680eedd2a9ef
kirraObj <c@skymc.me>
2022-07-19 15:36:02 +0800

    update.

--- platform/platform-bukkit/src/main/java/net/sakuragame/eternal/kirracore/bukkit/command/management/CommandRestart.java
+++ platform/platform-bukkit/src/main/java/net/sakuragame/eternal/kirracore/bukkit/command/management/CommandRestart.java
@@ -7,7 +7,7 @@ import org.bukkit.command.CommandSender;
 @CommandMeta(label = {"kRestart"}, permission = "admin")
 public class CommandRestart {
 
-    public void execute(CommandSender sender, Integer delaySeconds, String reason) {
+    public void execute(CommandSender sender, int delaySeconds, String reason) {
         FunctionRestart.execute(delaySeconds, reason);
     }
 }
