658d34b83fd5138430ebd01152714a7b72f01424
kirraObj <c@skymc.me>
2022-08-02 14:40:03 +0800

    update.

---
+++ platform/platform-bukkit/src/main/java/net/sakuragame/eternal/kirracore/bukkit/command/CommandUntransform.java
@@ -0,0 +1,26 @@
+package net.sakuragame.eternal.kirracore.bukkit.command;
+
+import com.qrakn.honcho.command.CommandMeta;
+import lombok.val;
+import net.sakuragame.eternal.kirracore.bukkit.KirraCoreBukkit;
+import org.bukkit.Bukkit;
+import org.bukkit.command.CommandSender;
+
+@SuppressWarnings("SpellCheckingInspection")
+@CommandMeta(label = "hub", async = true)
+public class CommandUntransform {
+
+    public void execute(CommandSender sender, String playerID) {
+        val player = Bukkit.getPlayer(playerID);
+        if (player == null) {
+            return;
+        }
+        val profile = KirraCoreBukkit.getInstance().getProfileManager().getProfile(player);
+        if (profile == null) {
+            return;
+        }
+        profile.setConvertModel(null);
+        profile.convertMinutes = 0;
+        profile.loadConvertData();
+    }
+}
